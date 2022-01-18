3992bf34e634368e1e68735ce070fd85aed41fe7
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-01-18 17:39:34 +0900

    add: deleteToDo()

--- practice/js/todo.js
+++ practice/js/todo.js
@@ -2,6 +2,7 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
@@ -9,12 +10,23 @@ function handleToDoSubmit(event){
     paintToDo(newToDo);
 }
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newtodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newtodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
+
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
