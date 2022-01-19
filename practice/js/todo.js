4d5ad5b7165483aef0605878323cc2e7a5f2ad9b
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-01-19 11:52:13 +0900

    fix: deleteToDo( )
    todo 삭제 시 localStorage 반영

--- practice/js/todo.js
+++ practice/js/todo.js
@@ -13,20 +13,27 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newToDo);
-    toDos.push(newToDo);
+    const newToDoObj ={
+        text : newToDo,
+        id : Date.now()
+    };
+    paintToDo(newToDoObj);
+    toDos.push(newToDoObj);
     saveToDos();
 }
 
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter(todo => todo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newtodo){
     const li = document.createElement("li");
+    li.id = newtodo.id;
     const span = document.createElement("span");
-    span.innerText = newtodo;
+    span.innerText = newtodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
